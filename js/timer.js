029b174c805d6ff51a614c7dec251cf444fa7478
real0131 <real0131@gmail.com>
2015-12-18 19:57:39 +0900

    page css change

--- js/timer.js
+++ js/timer.js
@@ -1,13 +1,22 @@
+var timeTo = 1454198400;
+var time =Date.now();
+var result = timeTo - time;
+var time_day =document.getElementById('time_day'); 
+var time_hour=document.getElementById('time_hour');
+var time_min =document.getElementById('time_min');
+var time_sec =document.getElementById('time_sec');
 
-
-var timeto = function(timeA,timeB){if( timeA > timeB ){return (timeB+12)-timeA}else{return timeB-timeA} }
-
-
-
-var time = new Date();
-var to = {'year':2016,'month':1,'day':31};
-document.write(timeto(to.month,time.getMonth));
-
+/*
+time_day.write(result/86400);           
+time_hour.write(result%86400/3600);     
+time_min.write(result%86400%3600/60);   
+time_sec.write(result%86400%3600%60/60);
+*/
+console.log(Math.abs(timeTo-time));
+console.log((result/86400));           
+console.log(result%86400/3600);     
+console.log(result%86400%3600/60);   
+console.log(result%86400%3600%60/60);
 
 
 
