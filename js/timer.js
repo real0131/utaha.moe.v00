fdd5a3bbf1bf96db2ed2481ca89263106b575948
real0131 <real0131@gmail.com>
2015-12-22 15:13:29 +0900

    timer_finish

--- js/timer.js
+++ js/timer.js
@@ -1,10 +1,3 @@
-var timeTo = 1454198400;
-var time =Date.now();
-var result = (timeTo) - (time/1000);
-var time_day =document.getElementById('time_day'); 
-var time_hour=document.getElementById('time_hour');
-var time_min =document.getElementById('time_min');
-var time_sec =document.getElementById('time_sec');
 
 /*
 time_day.write(result/86400);           
@@ -12,11 +5,20 @@ time_hour.write(result%86400/3600);
 time_min.write(result%86400%3600/60);   
 time_sec.write(result%86400%3600%60/60);
 */ 
-time_day.write(Math.floor(result/86400));          
-time_hour.write(Math.floor(result%86400/3600));    
-time_min.write(Math.floor(result%86400%3600/60));  
-time_sec.write(Math.floor(result%86400%3600%60));
+var x = function()  {
+var timeTo = 1454198400;
+var time =Date.now();
+var result = (timeTo) - (time/1000);
+    
 
+document.getElementById('time_day').innerHTML=Math.floor(result/86400);          
+document.getElementById('time_hour').innerHTML=Math.floor(result%86400/3600);    
+document.getElementById('time_min').innerHTML=Math.floor(result%86400%3600/60);  
+document.getElementById('time_sec').innerHTML=Math.floor(result%86400%3600%60);
+
+};
+
+setInterval(x,1000);
 
 
 
