e5e74b1e7bd0adefc03a1d07dc9d0221a2cfedb4
real0131 <real0131@gmail.com>
2015-12-22 14:50:20 +0900

    timer

--- js/timer.js
+++ js/timer.js
@@ -1,6 +1,6 @@
 var timeTo = 1454198400;
 var time =Date.now();
-var result = timeTo - time;
+var result = (timeTo) - (time/1000);
 var time_day =document.getElementById('time_day'); 
 var time_hour=document.getElementById('time_hour');
 var time_min =document.getElementById('time_min');
@@ -11,12 +11,13 @@ time_day.write(result/86400);
 time_hour.write(result%86400/3600);     
 time_min.write(result%86400%3600/60);   
 time_sec.write(result%86400%3600%60/60);
-*/
-console.log(Math.abs(timeTo-time));
-console.log((result/86400));           
-console.log(result%86400/3600);     
-console.log(result%86400%3600/60);   
-console.log(result%86400%3600%60/60);
+*/ 
+time_day.write(Math.floor(result/86400));          
+time_hour.write(Math.floor(result%86400/3600));    
+time_min.write(Math.floor(result%86400%3600/60));  
+time_sec.write(Math.floor(result%86400%3600%60));
+
+
 
 
 
